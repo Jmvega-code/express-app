28801bcf1f533e9a27299a600d6da65a92b7c714
Juanma Vega <juanmavega83@gmail.com>
2020-01-25 16:05:55 +0100

    first commit/package.json and Nodemon (server restart

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+
+// "/" => "Hi there"
+app.get("/", function(req, res){
+  res.send("Hi there");
+});
+// "/bye" => "Goodbye"
+  app.get("/bye", function(req, res){
+    res.send("Goodbye");
+  });
+  // "/dog" => "Meow!"
+    app.get("/dog", function(req, res){
+      res.send("Meow!");
+    });
+    app.get("*", function(req, res){
+      res.send("Error 404: Page not found");
+    });
+
+// Tell express to listen for request
+app.listen(3000, function(){
+  console.log("Serving on port 3000")
+});
