d1e0a03c8b9a8b18cc0a23023b0c8c2b05ce83e8
Juanma Vega <juanmavega83@gmail.com>
2020-01-25 16:12:34 +0100

    routing order matters

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 var express = require("express");
 var app = express();
 
+app.get("*", function(req, res){
+  res.send("Error 404: Page not found");
+});
 // "/" => "Hi there"
 app.get("/", function(req, res){
   res.send("Hi there");
 });
+
 // "/bye" => "Goodbye"
-  app.get("/bye", function(req, res){
-    res.send("Goodbye");
-  });
-  // "/dog" => "Meow!"
-    app.get("/dog", function(req, res){
-      res.send("Meow!");
-    });
-    app.get("*", function(req, res){
-      res.send("Error 404: Page not found");
-    });
+app.get("/bye", function(req, res){
+res.send("Goodbye");
+});
+
+// "/dog" => "Meow!"
+app.get("/dog", function(req, res){
+  res.send("Meow!");
+});
+
 
 // Tell express to listen for request
 app.listen(3000, function(){
