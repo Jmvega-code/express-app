d44d92f3747c9cdf4b8ed5f7b3a778cbf3529bbd
Juanma Vega <juanmavega83@gmail.com>
2020-01-25 18:08:42 +0100

    set routing params

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ var app = express();
 
 // "/" => "Hi there"
 app.get("/", function(req, res){
-  res.send("Hi there");
+res.send("Hi there");
 });
 // "/bye" => "Goodbye"
-  app.get("/bye", function(req, res){
-    res.send("Goodbye");
-  });
-  // "/dog" => "Meow!"
-    app.get("/dog", function(req, res){
-      res.send("Meow!");
-    });
-    app.get("*", function(req, res){
-      res.send("Error 404: Page not found");
-    });
+app.get("/bye", function(req, res){
+res.send("Goodbye");
+});
+// "/dog" => "Meow!"
+app.get("/dog", function(req, res){
+res.send("Meow!");
+});
+app.get("*", function(req, res){
+res.send("Error 404: Page not found");
+});
 
 // Tell express to listen for request
 app.listen(3000, function(){
