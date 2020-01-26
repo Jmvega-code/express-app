f700e58e13a05b8faa42ee6cc5cb8e81c8d95c03
Juanma Vega <juanmavega83@gmail.com>
2020-01-26 12:45:23 +0100

    Express dinamic routing check

--- app.js
+++ app.js
@@ -3,18 +3,36 @@ var app = express();
 
 // "/" => "Hi there"
 app.get("/", function(req, res){
-res.send("Hi there");
+  res.send("Hi there");
 });
 // "/bye" => "Goodbye"
 app.get("/bye", function(req, res){
-res.send("Goodbye");
+  res.send("Goodbye");
 });
 // "/dog" => "Meow!"
-app.get("/dog", function(req, res){
-res.send("Meow!");
+app.get("/speak/:animal/", function(req, res){
+  var sounds = {
+    dog: "woof",
+    pig: "oink",
+    cat: "meow",
+    frog: "croak"
+  }
+  var animal = req.params.animal;
+
+  if(sounds.hasOwnProperty(animal)){
+    res.send("The " + animal + " says " + sounds[animal]);
+  } else {
+    res.send("We haven't herad of that animal");
+  }
 });
+app.get("/repeat/:word/:num", function(req, res){
+  var word = req.params.word + " ";
+  var num = req.params.num;
+  res.send(word.repeat(num));
+});
+
 app.get("*", function(req, res){
-res.send("Error 404: Page not found");
+  res.send("Error 404: Page not found");
 });
 
 // Tell express to listen for request
